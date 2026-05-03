aed42d25e55de845252e1d8cc633d40257ec8160
adnakscodes <darthwhite005@gmail.com>
2026-05-03 22:40:38 +0530

    Fix CORS origin mismatch

--- src/functions/cors.js
+++ src/functions/cors.js
@@ -1,6 +1,6 @@
 function corsHeaders() {
     return {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "http://localhost:8080",
         "Access-Control-Allow-Methods": "GET,POST,OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type"
     };
